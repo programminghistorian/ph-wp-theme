9ff6a8983fd989e76efcae6e59d4800e5c46a638
Jeremy Boggs <jeremy@clioweb.org>
2012-04-12 10:49:08 -0400

    Adds Modernizr and such.

--- header.php
+++ header.php
@@ -14,9 +14,17 @@
   <link href='http://fonts.googleapis.com/css?family=Lato:300,700' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet'> 
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js"></script>
-
+    <!-- Modernizr and Friends -->
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/javascripts/modernizr.min.js"></script>
+    <script>
+      Modernizr.load([
+        {
+          test: Modernizr.mq(),
+          nope: ['<?php echo get_stylesheet_directory_uri(); ?>/javascripts/respond.min.js',
+          '<?php echo get_stylesheet_directory_uri(); ?>/javascripts/selectivizr.min.js']
+        }
+      ]);
+    </script>
   <?php wp_head(); ?>
 </head>
 <body>
