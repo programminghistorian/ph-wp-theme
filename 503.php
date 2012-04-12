3620ee51cd9120c2d9409bfa51879e65bfd9dcec
Jeremy Boggs <jeremy@clioweb.org>
2012-04-12 10:44:08 -0400

    Adds 503 template.

---
+++ 503.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Programming Historian</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+  </head>
+  <body>
+    <header role="banner">
+    
+	<h1><a href="<?php bloginfo('home'); ?>">Programming Historian</a></h1>
+    </header>
+    <div id="content" role="main">
+      <p>It'll be here before you know it!</p>
+    </div>
+  </body>
+</html>
