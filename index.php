a9b6487cf293a932a3a009a2c09ece22c7d43a42
Jeremy Boggs <jeremy@clioweb.org>
2012-07-06 10:49:52 -0400

    Adds date kicker and author info to posts.

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <article>
             <header>
+                <?php if (!is_page()): ?>
+                <p class="kicker"><?php echo the_time('F j, Y'); ?></p>
+                <?php endif; ?>
                 <h1><?php the_title(); ?></h1>
             </header>
             <div class="content">
@@ -14,6 +17,18 @@
               }
             ?>
             </div>
+            <?php if (is_single()): ?>
+            <footer>
+              <div class="author-info">
+                <a class="author-picture" href="<?php echo get_the_author_meta('user_url'); ?>"><?php echo get_avatar(get_the_author_meta('ID'),120); ?></a>
+                <p class="author-name">By <?php echo(get_the_author_meta('user_firstname') . ' ' . get_the_author_meta('user_lastname')); ?></p>
+                <?php if ($description = get_the_author_meta('user_description')): ?>
+                <div class="author-description"><?php echo wpautop($description); ?></div>
+                <?php endif; ?>
+                <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" class="all-posts">See all posts by <?php echo get_the_author_meta('user_firstname'); ?></a>
+              </div>
+            </footer>
+            <?php endif; ?>
           </article>
       <?php endwhile; else: ?>
           <div id="not-found">
