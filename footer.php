b1442e2625a06181314665465aa6c4de38135408
Jeremy Boggs <jeremy@clioweb.org>
2012-07-06 11:31:25 -0400

    Removes list of lessons from footer.

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 </div>
 <footer role="contentinfo">
-<?php echo ph_list_lessons(); ?>
 <?php wp_nav_menu( array( 'theme_location' => 'footer_additional', 'depth' => 1, 'container_class' => 'additional', 'menu_class' => 'additional'  ) ); ?>
 
 </footer>
