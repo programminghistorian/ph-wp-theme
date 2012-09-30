940635e8f557041a549d567a2e5db8ccd886c484
Jeremy Boggs <jeremy@clioweb.org>
2012-09-30 18:22:39 -0400

    Adds CC-BY license.
    Fixes #7.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div>
 <footer role="contentinfo">
 <?php wp_nav_menu( array( 'theme_location' => 'footer_additional', 'depth' => 1, 'container_class' => 'additional', 'menu_class' => 'additional'  ) ); ?>
-
+<p><em><?php bloginfo('name'); ?></em> released under the <a href="http://creativecommons.org/licenses/by/2.0/" rel="license">CC-BY</a> license.</p>
 </footer>
 <?php wp_footer(); ?>
 </body>
