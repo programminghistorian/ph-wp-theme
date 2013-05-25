249396df4cb0e349f9abaf73bed1c1dded478ed5
Jeremy Boggs <jeremy@clioweb.org>
2013-05-25 17:21:13 -0400

    Sorts lessons by category, then hierarchically.
    Removes columns on lessons page in favor of a single list by category.

--- archive-lesson.php
+++ archive-lesson.php
@@ -1,8 +1,38 @@
 <?php get_header(); ?>
 <h1>Lessons</h1>
-<ul class="lessons">
-<?php wp_list_pages('post_type=lesson&title_li='); ?>
-</ul>
+<?php
+global $wp_query;
+
+$categories = get_categories(array('taxonomy' => 'lesson_topics'));
+
+// This is ugly terrible.
+
+foreach ($categories as $category) {
+    echo '<section class="'.$category->slug.' category">';
+    echo "<h2>".$category->name."</h2>";
+    $args = array_merge( $wp_query->query_vars, array('post_parent' => 0, 'lesson_topics' => $category->slug));
+    query_posts( $args );
+    if ( have_posts() ):
+      echo '<ul class="lessons">';
+      while( have_posts() ) : the_post();
+        echo '<li>';
+        the_title();
+        $html = wp_list_pages('echo=0&post_type=lesson&title_li=&child_of='.get_the_ID());
+        if ($html) {
+          echo '<ul>';
+          echo $html;
+          echo '</ul>';
+        }
+        echo '</li>';
+      endwhile;
+      echo '</ul>';
+    endif;
+    echo '</section>';
+}
+
+wp_reset_query();
+?>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
 
